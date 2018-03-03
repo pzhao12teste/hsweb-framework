55a2595dde1e04233d1f332a00f4ea3276429754
DrRacket <zpy1995@gmail.com>
2018-03-03 07:20:55 +0000

    164311ec9815 replay gib_eks

--- hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/RestControllerExceptionTranslator.java
+++ hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/RestControllerExceptionTranslator.java
@@ -36,6 +36,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -79,7 +81,7 @@ public class RestControllerExceptionTranslator {
     @ExceptionHandler(UnAuthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ResponseBody
-    ResponseMessage handleException(UnAuthorizedException exception) {
+    ResponseMessage handleException(UnAuthorizedException exception, HttpServletResponse response) {
         return ResponseMessage.error(401, exception.getMessage()).result(exception.getState());
     }
 
@@ -116,7 +118,15 @@ public class RestControllerExceptionTranslator {
     @ResponseBody
     ResponseMessage handleException(RuntimeException exception) {
         logger.error(exception.getMessage(), exception);
-        return ResponseMessage.error(400, exception.getMessage());
+        return ResponseMessage.error(500, exception.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    ResponseMessage handleException(SQLException exception) {
+        logger.error(exception.getMessage(), exception);
+        return ResponseMessage.error(500, "服务器内部错误");
     }
 
 
